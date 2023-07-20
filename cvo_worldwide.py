68abee6970834d11591b7d274d4b0a1a0e6dd9c1
shadow-1212 <sitrakarinjakatovonirina@gmail.com>
2023-07-20 18:47:05 +0300

    finish adding ratio per country

--- cvo_worldwide.py
+++ cvo_worldwide.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./hale-carport-304214-d9b788ff294c.json"
+
 
 # create function get datarame for a country_code
 POPULATION_PER_COUNTRY = {
@@ -13,16 +15,16 @@ POPULATION_PER_COUNTRY = {
     'CN': 1409517397
 }
 
-CONSTANT_FACTOR = 100000
+CONSTANT_FACTOR = 1000000
 
 
 def get_country_data(country_code):
     # connect to big query and load the data and use json credentials
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./hale-carport-304214-d9b788ff294c.json"
     client = bigquery.Client()
     # Perform a query.
     QUERY = (
-        'SELECT * FROM `hale-carport-304214.worldwide_covid19_after_2020.worldwide_covid19_after_2020` WHERE country_code = "' + country_code + '" ORDER BY date ASC'
+        'SELECT * FROM `hale-carport-304214.worldwide_covid19_after_2020.worldwide_covid19_after_2020` WHERE country_code = "' +
+        country_code + '" ORDER BY date ASC'
     )
     result = client.query(QUERY).to_dataframe()  # API request
     # transform the result into a dataframe
@@ -36,7 +38,7 @@ def get_country_data(country_code):
         [pd.Grouper(key='date', freq='M')]).sum().fillna(0).reset_index()
     # Calculate recovery rate with condition to handle division by zero
     country_monthly_data['recovery_rate'] = np.where(
-        country_monthly_data['new_confirmed'] == 0, 1, 1 - country_monthly_data['new_deceased'] / country_monthly_data['new_confirmed'])
+        country_monthly_data['new_confirmed'] == 0, 1, 1 - (country_monthly_data['new_deceased'] / country_monthly_data['new_confirmed']))
 
     # country_monthly_data['recovery_rate'] = country_monthly_data.apply(
     #     lambda row: 100 if row['new_deceased'] == 0 else row['recovery_rate'], axis=1)
@@ -44,8 +46,8 @@ def get_country_data(country_code):
     country_monthly_data = country_monthly_data[country_monthly_data['new_confirmed'] != 0]
 
     # ratio the recovery rate to the population
-    country_monthly_data['recovery_rate'] = country_monthly_data['recovery_rate'] * \
-        CONSTANT_FACTOR / POPULATION_PER_COUNTRY[country_code]
+    country_monthly_data['recovery_rate'] = (country_monthly_data['recovery_rate'] *
+                                             CONSTANT_FACTOR) / POPULATION_PER_COUNTRY[country_code]
 
     # export the monthly_data to a csv file inside CSV folder
     country_monthly_data.to_csv(
