b4fee45de58ff47d7b1c79589aa88d4aa8271fb9
shadow-1212 <sitrakarinjakatovonirina@gmail.com>
2023-07-20 17:11:17 +0300

    create function to get all datafram per coutries

--- cvo_worldwide.py
+++ cvo_worldwide.py
@@ -4,25 +4,51 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# connect to big query and load the data and use json credentials
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./hale-carport-304214-d9b788ff294c.json"
-client = bigquery.Client()
-# Perform a query.
-QUERY = (
-    'SELECT * FROM `hale-carport-304214.worldwide_covid19_after_2020.worldwide_covid19_after_2020` '
-)
-data = client.query(QUERY).to_dataframe  # API request
-dataframe = pd.DataFrame(data)
-# export the data to a csv file
-# Aggregate data on a monthly basis  by country and fill in missing values with 0
-# monthly_data = data.groupby(
-#     [pd.Grouper(key='date', freq='M'), 'country_name']).sum().fillna(0).reset_index()
-# # Calculate recovery rate with condition to handle division by zero
-# monthly_data['recovery_rate'] = monthly_data['new_confirmed'] / \
-#     monthly_data['new_deceased']
-# monthly_data['recovery_rate'] = monthly_data.apply(
-#     lambda row: 100 if row['new_deceased'] == 0 else row['recovery_rate'], axis=1)
-# # delete all rows with new_confirmed = 0 and new_deceased = 0
-# monthly_data = monthly_data[monthly_data['new_confirmed'] != 0]
-# # export the monthly_data to a csv file
-# monthly_data.to_csv('monthly_data.csv', index=False)
+# create function get datarame for a country_code
+
+
+def get_country_data(country_code, dataframe):
+    # get the dataframe for the country
+    country = dataframe[dataframe['country_code'] == country_code]
+    # get the monthly data for the country
+    country_monthly_data = country.groupby(
+        [pd.Grouper(key='date', freq='M')]).sum().fillna(0).reset_index()
+    # Calculate recovery rate with condition to handle division by zero
+    country_monthly_data['recovery_rate'] = country_monthly_data['new_confirmed'] / \
+        country_monthly_data['new_deceased']
+    country_monthly_data['recovery_rate'] = country_monthly_data.apply(
+        lambda row: 100 if row['new_deceased'] == 0 else row['recovery_rate'], axis=1)
+    # delete all rows with new_confirmed = 0 and new_deceased = 0
+    country_monthly_data = country_monthly_data[country_monthly_data['new_confirmed'] != 0]
+    # export the monthly_data to a csv file
+    country_monthly_data.to_csv(
+        country_code + '_monthly_data.csv', index=False)
+    return country_monthly_data
+
+
+# function
+if __name__ == "__main__":
+    # connect to big query and load the data and use json credentials
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./hale-carport-304214-d9b788ff294c.json"
+    client = bigquery.Client()
+    # Perform a query.
+    QUERY = (
+        'SELECT * FROM `hale-carport-304214.worldwide_covid19_after_2020.worldwide_covid19_after_2020` '
+    )
+    result = client.query(QUERY).to_dataframe()  # API request
+    # transform the result into a dataframe
+    dataframe = pd.DataFrame(result)
+    # convert the date column to datetime type
+    dataframe['date'] = pd.to_datetime(dataframe['date'])
+    # define an array of country codes
+    country_codes = ['US', 'FR', 'MG', 'GB', 'CN']
+    # create a dataframe for each country and plot all the recovery rates on the same graph
+    for country_code in country_codes:
+        country_monthly_data = get_country_data(country_code, dataframe)
+        plt.plot(country_monthly_data['date'],
+                 country_monthly_data['recovery_rate'],
+                 label=country_code, marker='o')
+    plt.xlabel('Date')
+    plt.ylabel('Recovery Rate')
+    plt.legend()
+    plt.show()
